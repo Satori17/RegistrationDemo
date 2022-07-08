dd4c53ea04185c8ec249c8547312cdee93fd8099
saba khitaridze <sabaxitara@icloud.com>
2022-07-09 00:42:18 +0400

    Initial Commit

---
+++ Saba_Khitaridze_Task_14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Saba_Khitaridze_Task_14
+//
+//  Created by Saba Khitaridze on 09.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
